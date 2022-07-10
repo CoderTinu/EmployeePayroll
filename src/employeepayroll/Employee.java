39f0d50d792d74dcf4fcf63dca66aabe198157ad
Bhagyashri <bhagyashrijulme70@gmail.com>
2022-07-10 23:09:55 +0530

    Able to read and write data

---
+++ src/employeepayroll/Employee.java
@@ -0,0 +1,23 @@
+package employeepayroll;
+
+public class Employee {
+	private String name;
+    private byte id;
+    private long salary;
+
+    public Employee(String name, byte id, long salary) {
+        this.name = name;
+        this.id = id;
+        this.salary = salary;
+    }
+
+    @Override
+    public String toString() {
+        return "Employee{" +
+                "name='" + name + '\'' +
+                ", id=" + id +
+                ", salary=" + salary +
+                '}';
+    }
+
+}
